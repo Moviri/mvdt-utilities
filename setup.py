a09b32e69a125b9bcdb3d571fe14e1f707207a97
loucask <139135545+loucask@users.noreply.github.com>
2025-01-10 11:33:08 -0500

    Workflow updates (#1)
    * Update workflow
    
    * workflow
    
    * workflow tag
    
    * workflow upload test
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow typo
    
    * workflow
    
    * workflow
    
    * debugging
    
    * debug
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * bump version
    
    * workflow
    
    * Update action to run on push to main

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mvdt_utilities",
-    version="0.1.0",
+    version="0.2.0",
     description="Moviri utilities for Dynatrace extension development.",
     author="Moviri",
     author_email="dynatrace_extensions@moviri.com",
